c60f7ef1489d5d1c1a1a059ccb438a80446d0650
Ling Huang <linghuang@Lings-MacBook-Pro.local>
2024-03-18 18:56:41 -0700

    second

---
+++ server/db.js
@@ -0,0 +1,10 @@
+const client = new pageXOffset.Client(process.env.DATABASE_URL || 'postgress://localhost ')
+
+const createUser = async() =>{}
+
+const createProduct = async() =>{}
+
+const selectOrder = async() =>{}
+
+const selectProduct = async() => {}
+
