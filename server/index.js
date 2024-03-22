8225b8fb449a02d5b7f972a9507c4ae7e5ec2d5d
Ling Huang <linghuang@Lings-MacBook-Pro.local>
2024-03-22 15:48:06 -0700

    SQL

--- server/index.js
+++ server/index.js
@@ -2,3 +2,27 @@ const pg = require('pg')
 const express = require('express')
 const client = new pg.Client(process.env.DATABASE_URL || 'postgres://localhost/the_acme_notes_db')
 const app = express()
+
+//Create an async function
+const init = async () => {
+    //Inside the code body of the function, first awat client.connect()
+    await client.connect();
+    console.log('connected to database');
+    let SQL = `DROP TABLE IF EXISTS notes;
+    CREATE TABLE notes(
+    id SERIAL PRIMARY KEY,
+    created_at TIMESTAMP DEFAULT now(),
+    updated_at TIMESTAMP DEFAULT now(),
+    ranking INTEGER DEFAULT 3 NOT NULL,
+    txt VARCHAR(255) NOT NULL
+    );`;
+    await client.query(SQL);
+    console.log('tables created');
+    SQL = `INSERT INTO notes(txt, ranking) VALUES('learn express', 5);
+    INSERT INTO notes(txt, ranking) VALUES('write SQL queries', 4);
+    INSERT INTO notes(txt, ranking) VALUES('create routes', 2);`;
+    await client.query(SQL);
+    console.log('data seeded');
+  };
+  
+  init();
