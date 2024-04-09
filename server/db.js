38658ce8509f9b6dd742e5bcbe6d9a41511e03f2
Ling Huang <linghuang@Lings-MacBook-Pro.local>
2024-04-08 18:06:51 -0700

    db

--- server/db.js
+++ server/db.js
@@ -1,5 +1,5 @@
 const pg = require('pg');
-const client = new pg.Client(process.env.DATABASE_URL || 'postgres://localhost/acme_auth_store_db');
+const client = new pg.Client(process.env.DATABASE_URL || 'postgres://localhost/ling_unit4_career_sim');
 const uuid = require('uuid');
 const bcrypt = require('bcrypt');
 
